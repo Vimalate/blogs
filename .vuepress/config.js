5f4929ef22a7b983780af9f1969eaa3ec9a14b00
3hliuj <1902644369@qq.com>
2022-12-30 16:29:24 +0800

    添加看板插件

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -63,12 +63,31 @@ module.exports = {
     //     "z16",
     //   ],
     // },
-    // "vuepress-plugin-nuggets-style-copy": {
-    //   copyText: "复制代码",
-    //   tip: {
-    //     content: "复制成功",
-    //   },
-    // },
+    // "vuepress-plugin-nuggets-style-copy": true,
+    'vuepress-plugin-helper-live2d': {
+      // 是否开启控制台日志打印(default: false)
+      log: false,
+      live2d: {
+        // 是否启用(关闭请设置为false)(default: true)
+        enable: true,
+        // 模型名称(default: hibiki)>>>取值请参考：
+        // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+        model: 'wanko',
+        display: {
+          position: "right", // 显示位置：left/right(default: 'right')
+          width: 135, // 模型的长度(default: 135)
+          height: 300, // 模型的高度(default: 300)
+          hOffset: 65, //  水平偏移(default: 65)
+          vOffset: 0, //  垂直偏移(default: 0)
+        },
+        mobile: {
+          show: false // 是否在移动设备上显示(default: false)
+        },
+        react: {
+          opacity: 0.8 // 模型透明度(default: 0.8)
+        }
+      }
+    }
   },
   "theme": "reco",
   "themeConfig": {
@@ -105,7 +124,7 @@ module.exports = {
           },
           {
             "text":"Vue-Library",
-            "link":"/docs/Vue-Library"
+            "link":"/docs/Vue-Library/"
           },
           {
             "text": "Other-Library",
@@ -178,7 +197,7 @@ module.exports = {
         "实现一个 new 操作符",
         "reduce 的妙用"
       ],
-      "/docs/Vue-Library": [
+      "/docs/Vue-Library/": [
         "",
         "$ref和$el区别",
         "常用mixin",
--- docs/Vue-Library/README.md
+++ docs/Vue-Library/README.md
@@ -1,6 +1,6 @@
 ---
-title: [Vue-Library]
-date: 2022-12-30
+title: Vue-Library
+date: 2020-05-29
 ---
 
 这里存放了关于VUE的学习笔记
